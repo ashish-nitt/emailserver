11bf37e44bbd571bc57c11818e784f8306ae5fe9
ashish-nitt <ashish_nitt@yahoo.co.in>
2018-05-20 20:04:37 +0530

    CerateConnection

--- src/main/java/com/sample/project/App.java
+++ src/main/java/com/sample/project/App.java
@@ -1,13 +1,28 @@
 package com.sample.project;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
 /**
  * Hello world!
  *
  */
-public class App 
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.sample.project")
+public class App extends SpringBootServletInitializer
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
     }
 }
